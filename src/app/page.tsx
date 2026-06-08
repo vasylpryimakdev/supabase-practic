a815bf86f89e42ca7bc57a5b381776ceec77cde7
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-09 00:51:56 +0300

    feat(auth): add Clerk auth sheet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ArrowDownToLine } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import { SignUp, SignIn } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
 
 function Homepage() {
+  const [openSheet, setOpenSheet] = useState(false);
+  const queryStrings = useSearchParams();
+  const form = queryStrings.get("form");
   return (
     <div className="home-parent py-10 px-20">
       <div className="flex justify-between items-center">
         <h1 className="Shey.Fit text-2xl font-bold text-white uppercase">
           <b>Shey.Fit</b>
         </h1>
-        <Button variant={"outline"}>Sign-in</Button>
+        <Button variant={"outline"} onClick={() => setOpenSheet(true)}>
+          Sign-in
+        </Button>
       </div>
 
       <div className="flex flex-col justify-center items-center h-[80vh] gap-7 mt-20">
@@ -31,6 +45,34 @@ function Homepage() {
           className="animate-bounce cursor-pointer mt-20"
         />
       </div>
+
+      <div id="plans">
+        <h1 className="text-2xl font-bold text-center text-white mt-20">
+          Our Plans
+        </h1>
+      </div>
+
+      <Sheet open={openSheet} onOpenChange={setOpenSheet}>
+        <SheetContent className="lg:min-w-[500px] flex items-center justify-center min-h-screen auth-parent">
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+          </SheetHeader>
+
+          {form === "sign-up" ? (
+            <SignUp
+              routing="hash"
+              signInUrl="/?form=sign-in"
+              fallbackRedirectUrl={"/account"}
+            />
+          ) : (
+            <SignIn
+              routing="hash"
+              signUpUrl="/?form=sign-up"
+              fallbackRedirectUrl={"/account"}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 }
