f61cfb6d7dd7a79836170dfee493501b3ff28da3
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-09 14:03:42 +0300

    feat: add user profile sync with Supabase

---
+++ src/actions/users.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import supabase from "@/config/supabase-config";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const getCurrentUserFromSupabase = async () => {
+  try {
+    // if the clerk user is present in the supabase database, then return the user , else create a new user and return the user
+    const clerkUser = await currentUser();
+
+    const { data, error } = await supabase
+      .from("user_profiles")
+      .select("*")
+      .eq("clerk_user_id", clerkUser?.id);
+
+    if (error) {
+      throw error;
+    }
+    if (data && data.length) {
+      return {
+        success: true,
+        data: data[0],
+      };
+    }
+
+    // create a new user in the supabase database
+    const newUserObj = {
+      clerk_user_id: clerkUser?.id,
+      email: clerkUser?.emailAddresses[0].emailAddress,
+      name: clerkUser?.firstName + " " + clerkUser?.lastName,
+      is_active: true,
+      is_admin: false,
+    };
+
+    const { data: newUser, error: newUserError } = await supabase
+      .from("user_profiles")
+      .insert([newUserObj])
+      .select("*");
+    if (newUserError) {
+      throw newUserError;
+    }
+
+    return {
+      success: true,
+      data: newUser[0],
+    };
+
+    throw new Error("User not found");
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+};
