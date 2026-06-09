59833ec72f9b6aff0e8422ac06f3b97b6d3d07a1
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-09 18:22:18 +0300

    refactor: restructure admin plans module and move utilities

--- src/app/(private)/account/admin/plans/page.tsx
+++ src/app/(private)/account/admin/plans/page.tsx
@@ -1,9 +1,16 @@
+import { getAllPlans } from "@/actions/plans";
 import { Button } from "@/components/ui/button";
 import PageTitle from "@/components/ui/page-title";
 import Link from "next/link";
 import React from "react";
+import PlansTable from "./_components/plans-table";
 
 async function AdminPlansPage() {
+  const response: any = await getAllPlans();
+  if (!response.success) {
+    return <div>{response.message}</div>;
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -12,6 +19,8 @@ async function AdminPlansPage() {
           <Link href="/account/admin/plans/add">Add Plan</Link>
         </Button>
       </div>
+
+      <PlansTable plans={response.data} />
     </div>
   );
 }
