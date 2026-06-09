f5d76b260a7b8fc4912b34afced0b531e9449908
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-09 14:34:06 +0300

    fix: roleback

--- src/custom-layout/header.tsx
+++ src/custom-layout/header.tsx
@@ -1,8 +1,36 @@
 import { IUser } from "@/interfaces";
+import { Menu } from "lucide-react";
 import React from "react";
+import MenuItems from "./menu-items";
 
 function Header({ user }: { user: IUser | null }) {
-  return <div>Header</div>;
+  const [openMenuItems, setOpenMenuItems] = React.useState<boolean>(false);
+  return (
+    <div className="flex items-center justify-between bg-primary px-5 py-6">
+      <h1 className="text-2xl font-bold text-white">
+        <b className="text-white">Shey.</b>
+        <b className="text-green-600">Fit</b>
+      </h1>
+
+      <div className="flex gap-5 items-center">
+        <h1 className="text-sm text-white">{user?.name}</h1>
+
+        <Menu
+          className="text-white cursor-pointer"
+          size={15}
+          onClick={() => setOpenMenuItems(true)}
+        />
+      </div>
+
+      {openMenuItems && user && (
+        <MenuItems
+          user={user}
+          openMenuItems={openMenuItems}
+          setOpenMenuItems={setOpenMenuItems}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Header;
