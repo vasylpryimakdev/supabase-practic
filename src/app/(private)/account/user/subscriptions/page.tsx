05dcd1537e801a3f3ef23eda8c4c574e7effaff3
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-09 21:04:42 +0300

    feat(subscriptions): add user subscriptions table and extend subscription types

--- src/app/(private)/account/user/subscriptions/page.tsx
+++ src/app/(private)/account/user/subscriptions/page.tsx
@@ -1,5 +1,104 @@
+"use client";
+import { getAllSubscriptionsOfUser } from "@/actions/subscriptions";
+import PageTitle from "@/components/ui/page-title";
+import usersGlobalStore, {
+  IUsersGlobalStore,
+} from "@/global-store/users-store";
+import { ISubscription } from "@/interfaces";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import dayjs from "dayjs";
+import Spinner from "@/components/ui/spinner";
+
 function UserSubscriptionsPage() {
-  return <div>UserSubscriptionsPage</div>;
+  const [subscriptions, setSubscriptions] = React.useState<ISubscription[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const { user } = usersGlobalStore() as IUsersGlobalStore;
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response: any = await getAllSubscriptionsOfUser(user?.id!);
+      if (!response.success) {
+        throw new Error(response.message);
+      }
+      setSubscriptions(response.data);
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const columns = [
+    "Subscription ID",
+    "Purchase Date",
+    "Start Date",
+    "End Date",
+    "Plan",
+    "Amount",
+    "Payment Id",
+  ];
+
+  return (
+    <div>
+      <PageTitle title="My Subscriptions" />
+
+      {loading && <Spinner parentHeight={150} />}
+
+      {!subscriptions.length && !loading && (
+        <p className="text-sm text-gray-600">
+          You do not have any subscription at the moment.
+        </p>
+      )}
+
+      {subscriptions.length > 0 && !loading && (
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-gray-100">
+              {columns.map((column) => (
+                <TableHead className="font-bold" key={column}>
+                  {column}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {subscriptions.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>
+                  {dayjs(item.created_at).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>
+                  {dayjs(item.start_date).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>
+                  {dayjs(item.end_date).format("MMM DD, YYYY")}
+                </TableCell>
+                <TableCell>{item.plan?.name}</TableCell>
+                <TableCell>$ {item.amount}</TableCell>
+                <TableCell>{item.payment_id}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </div>
+  );
 }
 
 export default UserSubscriptionsPage;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -22,3 +22,19 @@ export interface IPlan {
   created_at: string;
   updated_at: string;
 }
+
+export interface ISubscription {
+  id: string;
+  plan_id: string;
+  plan: IPlan; // run time data
+  user_id: string;
+  user: IUser; // run time data
+  payment_id: string;
+  start_date: string;
+  end_date: string;
+  is_active: boolean;
+  amount: number;
+  total_duration: number;
+
+  created_at: string;
+}
